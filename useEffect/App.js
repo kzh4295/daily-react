ee82bad63edf6b98a7bcfdfc9be8a57337e49f22
kzh4295 <kzh4295@naver.com>
2023-11-01 20:06:46 +0900

    useEffect 수정

--- useEffect/App.js
+++ useEffect/App.js
@@ -1,15 +1,23 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import './styles.css';
 
 function App() {
   const [position, setPosition] = useState(0);
 
-  useEffect(() => {
-    setInterval(() => {
-      const moveDistance = 5;
-      setPosition((prevPosition) => prevPosition + moveDistance);
-    }, 1000);
-  }, []);
+  // useEffect(() => {
+  //   setInterval(() => {
+  //     const MOVEDISTANCE = 5;
+  //     setPosition((prevPosition) => prevPosition + MOVEDISTANCE);
+  //   }, 1_000);
+  //   return () => {
+  //     clearInterval(id);
+  //   };
+  // }, []);
+
+  useInterval(() => {
+    const MOVEDISTANCE = 5;
+    setPosition((prevPosition) => prevPosition + MOVEDISTANCE);
+  }, 1_000);
 
   return (
     <div className='character' style={{ left: position + 'px' }}>
